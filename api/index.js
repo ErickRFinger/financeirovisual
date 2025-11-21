22b4c387d2fab8e1e864955187169ff45de8e5ed
ErickRFinger <erick.finger123@gmail.com>
2025-11-21 16:50:29 -0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@
 
 import app from '../backend/server.js';
 
-// Handler para o Vercel
-// O Express app pode ser usado diretamente como handler
-export default app;
+// Handler para o Vercel Serverless Functions
+// O Vercel espera uma função que recebe (req, res)
+export default function handler(req, res) {
+  return app(req, res);
+}
 
